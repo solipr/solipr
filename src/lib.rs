447f4d70ec8dfd5ef6dbbf10c09877cc35f9dd19
tipragot <me@tipr.fr>
2024-10-04 21:36:28 +0200

    Stack vector implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,6 @@
-//! An example Rust library project
+//! A rust-based version control systemc
 
-/// Adds two numbers together
-#[must_use]
-#[inline]
-pub const fn add(left: u64, right: u64) -> u64 {
-    left.wrapping_add(right)
-}
+#![feature(stmt_expr_attributes)]
+#![feature(const_slice_from_raw_parts_mut)]
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        assert_eq!(add(2, 2), 4);
-        assert_eq!(add(u64::MAX, 3), 2);
-    }
-}
+pub mod stack;
