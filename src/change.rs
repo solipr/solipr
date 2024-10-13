a9e8548101c8cc398da6a6f60d141287361696dc
tipragot <me@tipr.fr>
2024-10-13 10:04:53 +0200

    Adding the SingleId

--- src/change.rs
+++ src/change.rs
@@ -33,38 +33,99 @@ impl LineId {
     const LAST: Self = Self(Uuid::max());
 }
 
-/// A change.
+/// The identifier of the SVG modified by a [Change].
+///
+/// For more information, look at
+/// [the SVG documentation](https://github.com/solipr/solipr/blob/main/docs/svg.md).
+pub enum SingleId {
+    /// The [Change] updates the content of a line.
+    LineContent(FileId, LineId),
+
+    /// The [Change] updates the existence of a line.
+    LineExistence(FileId, LineId),
+
+    /// The [Change] updates the parent of a line.
+    LineChild(FileId, LineId),
+
+    /// The [Change] updates the child of a line.
+    LineParent(FileId, LineId),
+}
+
+/// A change that can be applied to a repository.
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Change {
+    /// The changes replaced by this change.
+    ///
+    /// If there is more than 3 changes to be replaced, you should make one
+    /// change to replace the first 3 changes and then make another change
+    /// to replace the first created change with the rest.
+    replace: StackVec<ChangeHash, 3>,
+
+    /// The content of the change.
+    content: ChangeContent,
+}
+
+impl Change {
+    /// The SVG modified by this change.
+    ///
+    /// For more information, look at
+    /// [the SVG documentation](https://github.com/solipr/solipr/blob/main/docs/svg.md).
+    #[must_use]
+    #[inline]
+    pub const fn single_id(&self) -> SingleId {
+        match self.content {
+            ChangeContent::LineContent {
+                file_id, line_id, ..
+            } => SingleId::LineContent(file_id, line_id),
+            ChangeContent::LineExistence {
+                file_id, line_id, ..
+            } => SingleId::LineExistence(file_id, line_id),
+            ChangeContent::LineParent {
+                file_id, line_id, ..
+            } => SingleId::LineParent(file_id, line_id),
+            ChangeContent::LineChild {
+                file_id, line_id, ..
+            } => SingleId::LineChild(file_id, line_id),
+        }
+    }
+}
+
+/// The content of a [Change].
 ///
 /// TODO: Add the changes to modify files.
 #[derive(Clone, Copy, PartialEq, Eq, Hash)]
-pub enum Change {
-    /// Add a line to a file.
-    DefineLine {
-        /// The file in which the line is added.
+pub enum ChangeContent {
+    /// Update the content of a line.
+    LineContent {
+        /// The file modified by this change.
         file_id: FileId,
 
-        /// The line that is added.
+        /// The line modified by this change.
         line_id: LineId,
 
-        /// The content of the line.
+        /// The new content of the line.
         content: ContentHash,
     },
 
-    /// Remove a line from a file.
+    /// Update the Existence of a line.
     ///
-    /// This change does not really remove the line, it only marks it as
+    /// This change alone cannot really remove the line, it only marks it as
     /// removed. To be fully removed, the parent and child lines must be updated
-    /// with the [`Change::LineChild`] and [`Change::LineParent`] changes
-    /// respectively to make them stop being linked to the removed line.
+    /// with the [`ChangeContent::LineChild`] and [`ChangeContent::LineParent`]
+    /// changes respectively to make them stop being linked to the removed
+    /// line.
     ///
     /// For more information, look at
     /// [the OVG documentation](https://github.com/solipr/solipr/blob/main/docs/ovg.md).
-    RemoveLine {
-        /// The file in which the line is removed.
+    LineExistence {
+        /// The file modified by this change.
         file_id: FileId,
 
-        /// The line that is removed.
+        /// The line modified by this change.
         line_id: LineId,
+
+        /// The new existence of the line.
+        existence: bool,
     },
 
     /// Update the parent of a line.
@@ -77,13 +138,6 @@ pub enum Change {
 
         /// The line whose parent is updated.
         line_id: LineId,
-
-        /// The changes replaced by this change.
-        ///
-        /// If there is more than 3 changes to be replaced, you should make one
-        /// change to replace the first 3 changes and then make another change
-        /// to replace the first created change with the rest.
-        replace: StackVec<ChangeHash, 3>,
 
         /// The new parent of the line.
         parent: LineId,
@@ -99,13 +153,6 @@ pub enum Change {
 
         /// The line whose child is updated.
         line_id: LineId,
-
-        /// The changes replaced by this change.
-        ///
-        /// If there is more than 3 changes to be replaced, you should make one
-        /// change to replace the first 3 changes and then make another change
-        /// to replace the first created change with the rest.
-        replace: StackVec<ChangeHash, 3>,
 
         /// The new child of the line.
         child: LineId,
