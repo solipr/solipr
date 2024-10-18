e0403d66c2071df0c1d238fd93a2ac284b985ba4
CoCoSol <solois.corentin@gmail.com>
2024-10-18 23:04:07 +0200

    Add memory module to registry.rs

---
+++ src/registry/memory.rs
@@ -0,0 +1,49 @@
+//! Defines a memory based [Registry].
+
+use std::collections::HashMap;
+use std::io::{self, Cursor, Read};
+use std::sync::{Arc, RwLock};
+
+use sha2::{Digest, Sha256};
+
+use super::{ContentHash, Registry};
+
+/// A memory based [Registry].
+pub struct MemoryRegistry {
+    /// The content of the registry.
+    content: RwLock<HashMap<ContentHash, Arc<[u8]>>>,
+}
+
+impl Registry for MemoryRegistry {
+    type Error = io::Error;
+
+    fn read(&self, hash: ContentHash) -> Result<Option<impl Read>, Self::Error> {
+        let Ok(data) = self.content.read() else {
+            return Err(io::Error::other("failed to read content".to_owned()));
+        };
+        let Some(content) = data.get(&hash) else {
+            return Ok(None);
+        };
+        Ok(Some(Cursor::new(Arc::clone(content))))
+    }
+
+    fn write(&self, mut content: impl Read) -> Result<ContentHash, Self::Error> {
+        // Read the content into memory
+        let mut buffer = Vec::new();
+        content.read_to_end(&mut buffer)?;
+
+        // Create a unique hash for the content
+        let mut hasher = Sha256::new();
+        hasher.update(&buffer);
+        let hash = hasher.finalize().into();
+
+        // Write the content into the registry
+        let Ok(mut data) = self.content.write() else {
+            return Err(io::Error::other("failed to write content".to_owned()));
+        };
+        data.insert(ContentHash(hash), buffer.into());
+
+        // Return the hash of the content
+        Ok(ContentHash(hash))
+    }
+}
