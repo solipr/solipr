08f878ceb275856e485fd938fed245e5354ac30a
CoCoSol <solois.corentin@gmail.com>
2024-10-19 16:25:08 +0200

    FIX ALL

--- src/registry/persistent.rs
+++ src/registry/persistent.rs
@@ -1,4 +1,4 @@
-//! Defines a persistent memory based [Registry].
+//! Defines a persistent implementation of [Registry].
 
 use std::fs::{self, File};
 use std::io::{self, Read, Write};
@@ -9,23 +9,26 @@ use sha2::{Digest, Sha256};
 
 use super::{ContentHash, Registry};
 
-/// A persistent memory based [Registry].
+/// A persistent implementation of [Registry].
 pub struct PersistentRegistry {
-    /// The path to the folder where the [Registry] is stored.
-    path: PathBuf,
+    /// The path to the folder where the contents are stored.
+    folder: PathBuf,
 }
 
 impl Registry for PersistentRegistry {
     type Error = io::Error;
 
     fn read(&self, hash: ContentHash) -> Result<Option<impl Read>, Self::Error> {
-        let encore_hash = BASE64_URL_SAFE_NO_PAD.encode(hash.0);
-
-        if encore_hash.len() < 2 {
-            return Err(io::Error::other("invalid content hash".to_owned()));
-        }
-        #[expect(clippy::string_slice, reason = "The length is checked above")]
-        match File::open(self.path.join(&encore_hash[0..2]).join(&encore_hash[2..])) {
+        let encoded_hash = BASE64_URL_SAFE_NO_PAD.encode(hash.0);
+        #[expect(
+            clippy::string_slice,
+            reason = "there is always more than 2 characters in a ContentHash"
+        )]
+        match File::open(
+            self.folder
+                .join(&encoded_hash[..2])
+                .join(&encoded_hash[2..]),
+        ) {
             Ok(file) => Ok(Some(file)),
             Err(err) if err.kind() == io::ErrorKind::NotFound => Ok(None),
             Err(err) => Err(err),
@@ -33,40 +36,46 @@ impl Registry for PersistentRegistry {
     }
 
     fn write(&self, mut content: impl Read) -> Result<ContentHash, Self::Error> {
-        let mut hasher = Sha256::new();
-
         // Create a temporary file to store the content in.
-        let temp_file_path = self.path.join(uuid::Uuid::now_v7().to_string());
-        let mut temp_file = File::create(self.path.join(temp_file_path.clone()))?;
+        let temp_file_path = self.folder.join(uuid::Uuid::now_v7().to_string());
+        let mut temp_file = File::create(self.folder.join(&temp_file_path))?;
 
-        // Loop 32 bytes at a time.
+        // Loop 32 bytes at a time and update the hasher
+        // until we reach the end of the content
+        let mut hasher = Sha256::new();
+        let mut buffer = [0; 32];
         loop {
-            let mut buffer = [0; 32];
-
-            match content.read(&mut buffer) {
+            let byte_count = match content.read(&mut buffer) {
                 Ok(0) => break,
-                Err(ref err) if err.kind() == io::ErrorKind::Interrupted => {}
-                Err(err) => {
-                    return Err(err);
-                }
-                _ => {}
-            }
-            hasher.update(buffer);
+                Ok(byte_count) => byte_count,
+                Err(ref err) if err.kind() == io::ErrorKind::Interrupted => continue,
+                Err(err) => return Err(err),
+            };
+            #[expect(
+                clippy::indexing_slicing,
+                reason = "byte_count is always smaller or equal than 32"
+            )]
+            hasher.update(&buffer[..byte_count]);
             temp_file.write_all(&buffer)?;
         }
 
+        // Create a unique hash for the content
         let hash = ContentHash(hasher.finalize().into());
         let encoded_hash = BASE64_URL_SAFE_NO_PAD.encode(hash.0);
 
+        // Move the temporary file into the correct location
         fs::rename(
             temp_file_path,
             #[expect(
                 clippy::string_slice,
-                reason = "There is always more than 2 characters in 32 bytes in base 64"
+                reason = "there is always more than 2 characters in 32 bytes encoded in base64"
             )]
-            self.path.join(&encoded_hash[0..2]).join(&encoded_hash[2..]),
+            self.folder
+                .join(&encoded_hash[..2])
+                .join(&encoded_hash[2..]),
         )?;
 
+        // Return the hash of the content
         Ok(hash)
     }
 }
