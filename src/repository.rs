3a77702bfd7ed3218e8ab815d5263c6444653262
Tipragot <me@tipr.fr>
2024-10-21 16:12:03 +0200

    Implement generating Changes to update an SVG (#73)

--- src/repository.rs
+++ src/repository.rs
@@ -12,6 +12,7 @@ use uuid::Uuid;
 
 use crate::change::{Change, ChangeContent, ChangeHash, FileId, LineId, SingleId};
 use crate::registry::ContentHash;
+use crate::stack::StackVec;
 
 pub mod persistent;
 
@@ -360,6 +361,33 @@ pub trait Repository<'manager> {
 
         // Return the graph
         Ok(FileGraph(graph))
+    }
+
+    /// Returns the changes needed to replace the current value of an SVG.
+    ///
+    /// # Errors
+    ///
+    /// An error will be returned if there was an error while doing the
+    /// operation.
+    fn svg_diff(&self, new_content: ChangeContent) -> Result<HashSet<Change>, Self::Error> {
+        let mut result = HashSet::new();
+        let mut heads = Vec::from_iter(self.heads(new_content.single_id())?);
+        while !heads.is_empty() {
+            let mut replaced_heads = StackVec::new();
+            while !heads.is_empty() && !replaced_heads.is_full() {
+                #[expect(clippy::unwrap_used, reason = "heads is not empty")]
+                replaced_heads.push(heads.pop().unwrap());
+            }
+            let change = Change {
+                replace: replaced_heads,
+                content: new_content,
+            };
+            result.insert(change);
+            if !heads.is_empty() {
+                heads.insert(0, change.calculate_hash());
+            }
+        }
+        Ok(result)
     }
 
     /// Applies the given [`Change`] to the repository and returns the hash of
