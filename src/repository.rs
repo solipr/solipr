cbc2d2577a4b6dc5fe9d0e26e19ee1a0e7ca6396
Tipragot <me@tipr.fr>
2024-10-29 12:08:58 +0100

    Create a structure for the final rendered file (#80)

--- src/repository.rs
+++ src/repository.rs
@@ -7,12 +7,14 @@ use std::collections::hash_map::Entry;
 use std::collections::{BTreeSet, HashMap, HashSet};
 use std::error::Error;
 use std::fmt::{self, Display};
+use std::io::{self, BufRead, BufReader, Read, Write};
 
 use borsh::{BorshDeserialize, BorshSerialize};
+use thiserror::Error;
 use uuid::Uuid;
 
 use crate::change::{Change, ChangeContent, ChangeHash, FileId, LineId, SingleId};
-use crate::registry::ContentHash;
+use crate::registry::{ContentHash, Registry};
 use crate::stack::StackVec;
 
 pub mod persistent;
@@ -636,4 +638,254 @@ impl<'graph> From<&'graph FileGraph> for AcyclicFileGraph<'graph> {
         );
         graph
     }
+}
+
+/// An error that can happen while rendering a [`LinearFile`] into a [Write].
+#[derive(Debug, Error)]
+pub enum LinearFileRenderError<Reg: Registry> {
+    /// An error that can happen while reading from the registry.
+    #[error("registry error: {0}")]
+    Registry(Reg::Error),
+
+    /// The given registry does not contain the given content.
+    #[error("content not found in the registry: {0}")]
+    NotFound(ContentHash),
+
+    /// An io error.
+    #[error("io error: {0}")]
+    Io(#[from] io::Error),
+}
+
+/// An error that can happen while parsing a [Read] into a [`LinearFile`].
+#[derive(Debug, Error)]
+pub enum LinearFileParseError<Reg: Registry> {
+    /// An error that can happen while writing to the registry.
+    #[error("registry error: {0}")]
+    Registry(Reg::Error),
+
+    /// An io error.
+    #[error("io error: {0}")]
+    Io(#[from] io::Error),
+}
+
+/// A representation of a file in the repository in a linear way.
+pub struct LinearFile(Vec<LinearFileLine>);
+
+impl LinearFile {
+    /// Render a [`LinearFile`] into a [Write].
+    ///
+    /// # Errors
+    ///
+    /// There can be an error if a content is not found in the registry or if
+    /// there is an io error while writing to the writer or reading from the
+    /// registry.
+    pub fn render<Reg: Registry>(
+        &self,
+        regitry: impl AsRef<Reg>,
+        mut writer: impl Write,
+    ) -> Result<(), LinearFileRenderError<Reg>> {
+        for (index, line) in self.0.iter().enumerate() {
+            if index > 0 {
+                writer.write_all(b"\n")?;
+            }
+            match *line {
+                LinearFileLine::Line(_, content) => {
+                    let mut content = regitry
+                        .as_ref()
+                        .read(content)
+                        .map_err(LinearFileRenderError::Registry)?
+                        .ok_or(LinearFileRenderError::NotFound(content))?;
+                    io::copy(&mut content, &mut writer)?;
+                }
+                LinearFileLine::Conflict(id, ref possible_paths) => {
+                    writeln!(writer, "<<<<<<< CONFLICT {id}")?;
+                    for (index, lines) in possible_paths.iter().enumerate() {
+                        if index > 0 {
+                            writeln!(writer, "=======")?;
+                        }
+                        for &(_, content) in lines {
+                            let mut content = regitry
+                                .as_ref()
+                                .read(content)
+                                .map_err(LinearFileRenderError::Registry)?
+                                .ok_or(LinearFileRenderError::NotFound(content))?;
+                            io::copy(&mut content, &mut writer)?;
+                            writeln!(writer)?;
+                        }
+                    }
+                    write!(writer, ">>>>>>> CONFLICT")?;
+                }
+                LinearFileLine::Cycle(id, ref lines) => {
+                    writeln!(writer, "<<<<<<< CYCLE {id}")?;
+                    for &(_, content) in lines {
+                        let mut content = regitry
+                            .as_ref()
+                            .read(content)
+                            .map_err(LinearFileRenderError::Registry)?
+                            .ok_or(LinearFileRenderError::NotFound(content))?;
+                        io::copy(&mut content, &mut writer)?;
+                        writeln!(writer)?;
+                    }
+                    write!(writer, ">>>>>>> CYCLE")?;
+                }
+            }
+        }
+        Ok(())
+    }
+
+    /// Parse a [Read] into a [`LinearFile`] and store new content in the
+    /// registry.
+    ///
+    /// # Errors
+    ///
+    /// There can be an error if there is an io error while writing to the
+    /// registry or if there is an io error while reading from the reader.
+    pub fn parse<Reg: Registry>(
+        regitry: impl AsRef<Reg>,
+        reader: impl Read,
+    ) -> Result<Self, LinearFileParseError<Reg>> {
+        let mut result = Vec::new();
+
+        // Parse all lines
+        let mut reader = BufReader::new(reader);
+        let mut line = vec![1];
+        let mut conflict = None;
+        let mut cycle = None;
+        while !line.is_empty() {
+            // Read the next line
+            line.clear();
+            reader.read_until(b'\n', &mut line)?;
+            let content = line.strip_suffix(b"\n").unwrap_or(&line);
+
+            // Check if we have a conflict
+            if let Some(id) = content.strip_prefix(b"<<<<<<< CONFLICT ") {
+                if conflict.is_none() && cycle.is_none() {
+                    if let Ok(id) = Uuid::try_parse_ascii(id) {
+                        conflict = Some((id, vec![Vec::new()]));
+                    }
+                    continue;
+                }
+            }
+
+            // Check if we change path in a conflict
+            if content == b"=======" {
+                if let Some((_, ref mut paths)) = conflict {
+                    paths.push(Vec::new());
+                    continue;
+                }
+            }
+
+            // Check if we end a conflict
+            if content == b">>>>>>> CONFLICT" {
+                if let Some((id, paths)) = conflict.take() {
+                    result.push(LinearFileLine::Conflict(id, paths));
+                    continue;
+                }
+            }
+
+            // Check if we have a cycle
+            if let Some(id) = content.strip_prefix(b"<<<<<<< CYCLE ") {
+                if conflict.is_none() && cycle.is_none() {
+                    if let Ok(id) = Uuid::try_parse_ascii(id) {
+                        cycle = Some((id, Vec::new()));
+                    }
+                    continue;
+                }
+            }
+
+            // Check if we end a cycle
+            if content == b">>>>>>> CYCLE" {
+                if let Some((id, lines)) = cycle.take() {
+                    result.push(LinearFileLine::Cycle(id, lines));
+                    continue;
+                }
+            }
+
+            // If we don't have a conflict or a cycle, we have a line
+            let content = regitry
+                .as_ref()
+                .write(content)
+                .map_err(LinearFileParseError::Registry)?;
+            if let Some((_, ref mut paths)) = conflict {
+                // SAFETY: `paths` is never empty
+                unsafe {
+                    paths
+                        .last_mut()
+                        .unwrap_unchecked()
+                        .push((LineId::UNKNOWN, content));
+                }
+            } else if let Some((_, ref mut lines)) = cycle {
+                lines.push((LineId::UNKNOWN, content));
+            } else {
+                result.push(LinearFileLine::Line(LineId::UNKNOWN, content));
+            }
+        }
+
+        // If a conflict is still open, we add it as normal lines
+        if let Some((id, paths)) = conflict {
+            // Add the conflict start
+            let content = regitry
+                .as_ref()
+                .write(format!("<<<<<<< CONFLICT {id}").as_bytes())
+                .map_err(LinearFileParseError::Registry)?;
+            result.push(LinearFileLine::Line(LineId::UNKNOWN, content));
+
+            // Add the paths
+            for (index, path) in paths.into_iter().enumerate() {
+                // Add the path separator
+                if index > 0 {
+                    let content = regitry
+                        .as_ref()
+                        .write(format!("======={path:?}").as_bytes())
+                        .map_err(LinearFileParseError::Registry)?;
+                    result.push(LinearFileLine::Line(LineId::UNKNOWN, content));
+                }
+
+                // Add all the lines
+                for (id, content) in path {
+                    result.push(LinearFileLine::Line(id, content));
+                }
+            }
+        }
+
+        // If a cycle is still open, we add it as normal lines
+        if let Some((id, lines)) = cycle {
+            // Add the cycle start
+            let content = regitry
+                .as_ref()
+                .write(format!("<<<<<<< CYCLE {id}").as_bytes())
+                .map_err(LinearFileParseError::Registry)?;
+            result.push(LinearFileLine::Line(LineId::UNKNOWN, content));
+
+            // Add all the lines
+            for (id, content) in lines {
+                result.push(LinearFileLine::Line(id, content));
+            }
+        }
+
+        // Return the parsed file
+        Ok(Self(result))
+    }
+}
+
+/// A line in a [`LinearFile`].
+pub enum LinearFileLine {
+    /// A normal line.
+    Line(LineId, ContentHash),
+
+    /// A conflict that contains all possible paths in the graph.
+    ///
+    /// Every conflict has a unique ID that is used to identify it. This ID
+    /// should not be modified by the user in any way. The content of the
+    /// conflict should not be modified too, the only way to change it is by
+    /// removing it.
+    Conflict(Uuid, Vec<Vec<(LineId, ContentHash)>>),
+
+    /// A cycle that contains all lines in the cycle.
+    ///
+    /// Every cycle has a unique ID that is used to identify it. This ID
+    /// should not be modified by the user in any way. The content of the
+    /// cycle should not be modified too, the only way to change it is by
+    /// removing it.
+    Cycle(Uuid, Vec<(LineId, ContentHash)>),
 }
