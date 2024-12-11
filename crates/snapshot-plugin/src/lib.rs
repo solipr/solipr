e3ebd868f942d7e0990ccc4e6088457ec8343c13
tipragot <me@tipr.fr>
2024-12-11 18:58:34 +0100

    Save

--- crates/snapshot-plugin/src/lib.rs
+++ crates/snapshot-plugin/src/lib.rs
@@ -1,7 +1,22 @@
+use base64::prelude::{BASE64_URL_SAFE_NO_PAD, Engine};
+use sha3::{Digest, Sha3_256};
+
 wit_bindgen::generate!({
     path: "../../wit",
     world: "file-plugin",
 });
+
+impl Change {
+    pub fn calculate_hash(&self) -> String {
+        let mut hasher = Sha3_256::new();
+        for used_content in &self.used_contents {
+            hasher.update(used_content.as_bytes());
+        }
+        hasher.update(&self.plugin_data);
+        let result = hasher.finalize();
+        BASE64_URL_SAFE_NO_PAD.encode(result)
+    }
+}
 
 struct Plugin;
 
@@ -10,17 +25,11 @@ impl Guest for Plugin {
         todo!()
     }
 
-    fn calculate_diff(content_hash: String) -> Change {
+    fn calculate_diff(content_hash: String) -> Option<Change> {
         todo!()
     }
 
     fn apply_change(change: Change) {
-        let timestamp = u128::from_be_bytes(
-            change
-                .plugin_data
-                .try_into()
-                .expect("invalid change timestamp"),
-        );
         todo!()
     }
 
