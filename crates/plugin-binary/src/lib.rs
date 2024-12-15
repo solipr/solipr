6b468621d3540db4552a2d40f3905174c5736618
tipragot <me@tipr.fr>
2024-12-15 15:48:25 +0100

    Utils

--- crates/plugin-binary/src/lib.rs
+++ crates/plugin-binary/src/lib.rs
@@ -1,27 +1,9 @@
 use std::collections::{BTreeSet, HashSet};
 
-use base64::prelude::{BASE64_URL_SAFE_NO_PAD, Engine};
-use sha3::{Digest, Sha3_256};
 use solipr::file_plugin::kv_store::{kv_keys, kv_write};
 use solipr::file_plugin::repository::{change_read, content_cut, content_read_at, content_size};
 
-wit_bindgen::generate!({
-    path: "../../wit",
-    world: "file-plugin",
-
-});
-
-impl Change {
-    pub fn calculate_hash(&self) -> String {
-        let mut hasher = Sha3_256::new();
-        for used_content in &self.used_contents {
-            hasher.update(used_content.as_bytes());
-        }
-        hasher.update(&self.plugin_data);
-        let result = hasher.finalize();
-        BASE64_URL_SAFE_NO_PAD.encode(result)
-    }
-}
+solipr_plugin::generate!("../../wit");
 
 struct Plugin;
 
