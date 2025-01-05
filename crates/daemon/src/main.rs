647aab14659afae373d64d7fc7e8447c53f80516
Tipragot <me@tipr.fr>
2025-01-05 11:22:18 +0100

    Making axum stop gracefully

--- crates/daemon/src/main.rs
+++ crates/daemon/src/main.rs
@@ -6,16 +6,52 @@
 use axum::Router;
 use axum::routing::get;
 use config::CONFIG;
-use tokio::io;
 use tokio::net::TcpListener;
+use tokio::{io, select, signal};
 
 mod config;
 
+/// Waits for a shutdown signal.
+#[expect(
+    clippy::redundant_pub_crate,
+    reason = "the select! macro generate this error"
+)]
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        #[expect(clippy::expect_used, reason = "we want to crash if this fails")]
+        signal::ctrl_c()
+            .await
+            .expect("failed to install ctrl+c handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        #[expect(clippy::expect_used, reason = "we want to crash if this fails")]
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    select! {
+        () = ctrl_c => (),
+        () = terminate => (),
+    }
+}
+
 #[tokio::main]
 async fn main() -> io::Result<()> {
+    println!("Starting Solipr daemon");
     let app = Router::new().route("/", get(hello_world));
     let listener = TcpListener::bind(CONFIG.listen_address).await?;
-    axum::serve(listener, app).await
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
+        .await?;
+    println!("Solipr daemon stopped");
+    Ok(())
 }
 
 /// Serves a simple "Hello, World!" message.
