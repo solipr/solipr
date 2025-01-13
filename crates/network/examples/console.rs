bcb6268ae14726af8c9e86ffd1a658480101b56d
Tipragot <me@tipr.fr>
2025-01-13 13:28:27 +0100

    Separate the network system to an other crate (#146)

---
+++ crates/network/examples/console.rs
@@ -0,0 +1,57 @@
+//! A simple example that runs a [`SoliprPeer`] with a simple cli interface to
+//! interact with the network.
+
+use solipr_network::SoliprPeer;
+use tokio::io::{AsyncBufReadExt, BufReader, stdin};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    println!("Starting peer!");
+    let mut peer = SoliprPeer::start().await?;
+    let mut lines = BufReader::new(stdin()).lines();
+    while let Some(line) = lines.next_line().await? {
+        match line.as_str() {
+            "stop" => break,
+            "status" => {
+                println!("PeerID: {}", peer.id());
+                let provided = peer.provided_keys();
+                if provided.is_empty() {
+                    println!("No keys provided");
+                } else {
+                    println!("Provided keys:");
+                    for key in provided {
+                        println!("- {}", String::from_utf8_lossy(key));
+                    }
+                }
+            }
+            line => match line.split_once(' ') {
+                Some(("provide", key)) => {
+                    if peer.provide_key(key).await? {
+                        println!("Provided key: {key}");
+                    } else {
+                        println!("Failed to provide key: {key}");
+                    }
+                }
+                Some(("unprovide", key)) => {
+                    peer.stop_providing_key(key).await?;
+                    println!("Unprovided key: {key}");
+                }
+                Some(("find", key)) => {
+                    let peers = peer.find_providers(key).await?;
+                    if peers.is_empty() {
+                        println!("No peers found");
+                    } else {
+                        println!("Found peers:");
+                        for peer in peers {
+                            println!("- {peer}");
+                        }
+                    }
+                }
+                _ => println!("Unknown command: {line}"),
+            },
+        }
+    }
+    println!("Stopping peer!");
+    peer.stop().await?;
+    Ok(())
+}
