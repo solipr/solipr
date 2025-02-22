e80959eeb0dd6cd8e1a59b267af140a0b59dd822
Tipragot <me@tipr.fr>
2025-02-22 11:43:23 +0100

    Making the database api sync (#166)

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -1,11 +1,9 @@
 //! The core systems of Solipr.
 
 use std::error::Error as StdError;
-use std::future::Future;
+use std::ops::Deref;
 
-use futures_core::Stream;
-
-/// A database that can be used to store and retrieve data using transactions.
+/// A database that can be used to store and retrieve bytes using transactions.
 pub trait Database {
     /// A fatal error that can be returned by the database.
     ///
@@ -13,63 +11,106 @@ pub trait Database {
     /// used again.
     type FatalError: StdError;
 
-    /// A transaction that can only be used to read data from the database.
-    type ReadTx: ReadTx;
-
-    /// A transaction that can read and write data to the database.
-    type WriteTx: WriteTx;
+    /// The type of transaction returned by the database.
+    type Tx<'db>: DatabaseTx<'db, FatalError = Self::FatalError>
+    where
+        Self: 'db;
 
     /// Open a new read-only transaction.
     ///
     /// There can be multiple read-only transactions open at the same time.
-    fn read_tx(&self) -> impl Future<Output = Result<Self::ReadTx, Self::FatalError>> + Send;
+    ///
+    /// # Errors
+    ///
+    /// This method should return an error if there is an fatal error that can't
+    /// be recovered.
+    fn read_tx(&self) -> Result<Self::Tx<'_>, Self::FatalError>;
 
     /// Open a new transaction that can both read and write data to the
     /// database.
     ///
-    /// There can be only one write transaction open at a time.
-    fn write_tx(&self) -> impl Future<Output = Result<Self::WriteTx, Self::FatalError>> + Send;
+    /// There can be only one write transaction open at a time. If a write
+    /// transaction is already open, then this function will block until it
+    /// has been closed before opening a new one.
+    ///
+    /// # Errors
+    ///
+    /// This method should return an error if there is an fatal error that can't
+    /// be recovered.
+    fn write_tx(&self) -> Result<Self::Tx<'_>, Self::FatalError>;
 }
 
-/// A read-only transaction that can be used to retrieve data from the database.
+/// A transaction on a [Database].
 ///
-/// A read-only transaction is a snapshot of the database at some point in time.
-/// There can be multiple read-only transactions open at the same time.
-pub trait ReadTx {
+/// The tranaction can be read-only or read-write dpending on whether it is
+/// opened using [`Database::read_tx`] or [`Database::write_tx`].
+pub trait DatabaseTx<'db>: Send {
     /// A fatal error that can be returned by the transaction.
     ///
     /// If this error is returned by the transaction, the transaction should not
     /// be used again.
     type FatalError: StdError;
 
-    /// Get a value from the database.
-    fn get(
-        &self,
-        key: impl AsRef<[u8]>,
-    ) -> impl Future<Output = Result<Option<Vec<u8>>, Self::FatalError>> + Send;
+    /// The [`Slice`] used when retrieving data from the database.
+    type Slice<'tx>: Slice<'tx>
+    where
+        Self: 'tx;
 
-    /// Get a [Stream] of all values in the database that start with the given
-    /// prefix.
+    /// Returns an [Iterator] over the keys starting by the given prefix with
+    /// their values.
     ///
-    /// If the prefix is empty, this will return all values in the database.
-    fn keys<'tx>(
-        &'tx self,
-        prefix: impl AsRef<[u8]> + 'tx,
-    ) -> impl Stream<Item = Result<Vec<u8>, Self::FatalError>> + Send + 'tx;
-}
+    /// # Errors
+    ///
+    /// The iterator should return an error if there is an fatal error that
+    /// can't be recovered.
+    fn keys(
+        &self,
+        prefix: impl AsRef<[u8]>,
+    ) -> impl Iterator<Item = Result<(Self::Slice<'_>, Self::Slice<'_>), Self::FatalError>>;
 
-/// A write transaction that can both read and write data to the database.
-///
-/// There can only be one write transaction open at a time.
-pub trait WriteTx: ReadTx {
+    /// Get a value from the database.
+    ///
+    /// # Errors
+    ///
+    /// This method should return an error if there is an fatal error that can't
+    /// be recovered.
+    fn get(&self, key: impl AsRef<[u8]>) -> Result<Option<Self::Slice<'_>>, Self::FatalError>;
+
     /// Put a value in the database. If there is already a value for this key,
     /// it will be overwritten.
     ///
     /// If the `value` is `None`, this will remove the existing value for the
     /// key.
+    ///
+    /// This method will return an error if the transaction is read-only.
+    ///
+    /// # Errors
+    ///
+    /// This method should return an error if the transaction is read-only or if
+    /// there is an fatal error that can't be recovered.
     fn put(
         &self,
         key: impl AsRef<[u8]>,
         value: Option<impl AsRef<[u8]>>,
-    ) -> impl Future<Output = Result<(), Self::FatalError>> + Send;
+    ) -> Result<(), Self::FatalError>;
+
+    /// Commit the transaction to the database.
+    ///
+    /// This method will apply all changes made in this transaction to the
+    /// database in a single operation.
+    ///
+    /// This method will return an error if the transaction is read-only.
+    ///
+    /// # Errors
+    ///
+    /// This method should return an error if the transaction is read-only or if
+    /// there is an fatal error that can't be recovered.
+    fn commit(self) -> Result<(), Self::FatalError>;
 }
+
+/// A trait for a slice of bytes given by a database.
+///
+/// This trait enables the [Database] implementation to perform additional
+/// actions when a retrieved value is dropped. It is also useful for avoiding
+/// the need to clone the data from the [Database].
+pub trait Slice<'tx>: Deref<Target = [u8]> {}
