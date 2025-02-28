1a782991cb7887d07bbe46939fe0d7171145f3e7
Tipragot <me@tipr.fr>
2025-02-28 20:31:24 +0000

    Add Display, Debug and FromStr to ContentHash

--- src/identifier.rs
+++ src/identifier.rs
@@ -1,5 +1,11 @@
 //! This module contains all the identifier types and utilities used in Solipr.
 
+use std::fmt::{self, Debug, Display};
+use std::hash::Hash;
+use std::str::FromStr;
+
+use anyhow::Context;
+
 /// The hash of a content stored in a [Registry](crate::storage::Registry).
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct ContentHash([u8; 32]);
@@ -15,5 +21,26 @@ impl ContentHash {
     #[must_use]
     pub const fn as_bytes(&self) -> &[u8; 32] {
         &self.0
+    }
+}
+
+impl Debug for ContentHash {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "C{}", bs58::encode(self.0).into_string())
+    }
+}
+
+impl Display for ContentHash {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "C{}", bs58::encode(self.0).into_string())
+    }
+}
+
+impl FromStr for ContentHash {
+    type Err = anyhow::Error;
+
+    fn from_str(mut value: &str) -> Result<Self, Self::Err> {
+        value = value.trim().strip_prefix("C").context("missing prefix")?;
+        Ok(Self(bs58::decode(value.as_bytes()).into_array_const()?))
     }
 }
