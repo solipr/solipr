328dcb660bc6336aa23d0a05e4809ef20a13338a
Tipragot <me@tipr.fr>
2025-03-07 08:59:17 +0100

    Implement HostDocument for ReadDocument and WriteDocument (#216)

--- src/plugin.rs
+++ src/plugin.rs
@@ -2,9 +2,14 @@
 
 #![allow(missing_docs)]
 
+use borsh::{BorshDeserialize, BorshSerialize};
 use wasmtime::component::bindgen;
 
 bindgen!({
     world: "document-plugin",
     trappable_imports: true,
+    additional_derives: [
+        BorshDeserialize,
+        BorshSerialize,
+    ],
 });
